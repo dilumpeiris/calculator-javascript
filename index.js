da739ab9cd231575b25dc3926af9d28febd81ad2
dilumpeiris <dilum.peiris@icloud.com>
2025-08-07 18:42:50 +0530

    made a simple ui, no functionality tho

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
 let x = 256;
 let y = 10;
 
-console.log(x);
+document.addEventListener('DOMContentLoaded', function () {
+	const calc_body = document.getElementById('calc-body');
+
+	for (let i = 0; i < 9; i += 3) {
+		const calc_row = document.createElement('div');
+		calc_row.classList.add(
+			'flex',
+			'flex-row',
+			'w-full',
+			'h-full',
+			'items-center',
+			'justify-center',
+			'm-1'
+		);
+
+		for (let j = 1; j <= 3; j++) {
+			const calc_sqr = document.createElement('div');
+			calc_sqr.classList.add(
+				'flex',
+				'bg-red-100',
+				'w-full',
+				'h-full',
+				'items-center',
+				'justify-center',
+				'm-1',
+				'text-5xl',
+				'font-bold'
+			);
+			calc_sqr.textContent = i + j;
+
+			calc_row.appendChild(calc_sqr);
+		}
+		calc_body.appendChild(calc_row);
+	}
+});

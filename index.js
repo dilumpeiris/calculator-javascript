ba957621aea9b3d162af7657619d66277bc91c7d
dilumpeiris <dilum.peiris@icloud.com>
2025-08-09 16:57:09 +0530

    keypad and operations buttons done -- ui

--- index.js
+++ index.js
@@ -1,10 +1,52 @@
-let x = 256;
-let y = 10;
-
 document.addEventListener('DOMContentLoaded', function () {
 	const calc_body = document.getElementById('calc-body');
+	const calc_operations = document.getElementById('calc-operations');
 
-	for (let i = 0; i < 9; i += 3) {
+	let operations = [
+		['add', '+'],
+		['minus', '-'],
+		['mult', 'x'],
+		['div', '/'],
+		['equal', '='],
+	];
+
+	let keypad = [
+		['AC', 'AC'],
+		['changeSign', '+/-'],
+		['percent', '%'],
+		['1', '1'],
+		['2', '2'],
+		['3', '3'],
+		['4', '4'],
+		['5', '5'],
+		['6', '6'],
+		['7', '7'],
+		['8', '8'],
+		['9', '9'],
+		['brack', '()'],
+		['0', '0'],
+		['dot', '.'],
+	];
+
+	for (let i = 0; i < 5; i++) {
+		const calc_sqr = document.createElement('div');
+		calc_sqr.classList.add(
+			'flex',
+			'bg-red-100',
+			'w-full',
+			'h-full',
+			'items-center',
+			'justify-center',
+			'm-1',
+			'text-5xl',
+			'font-bold'
+		);
+
+		calc_sqr.textContent = operations[i][1];
+		calc_operations.appendChild(calc_sqr);
+	}
+
+	for (let i = 0; i < 15; i += 3) {
 		const calc_row = document.createElement('div');
 		calc_row.classList.add(
 			'flex',
@@ -16,7 +58,7 @@ document.addEventListener('DOMContentLoaded', function () {
 			'm-1'
 		);
 
-		for (let j = 1; j <= 3; j++) {
+		for (let j = i; j < i + 3; j++) {
 			const calc_sqr = document.createElement('div');
 			calc_sqr.classList.add(
 				'flex',
@@ -29,8 +71,8 @@ document.addEventListener('DOMContentLoaded', function () {
 				'text-5xl',
 				'font-bold'
 			);
-			calc_sqr.textContent = i + j;
 
+			calc_sqr.textContent = keypad[j][1];
 			calc_row.appendChild(calc_sqr);
 		}
 		calc_body.appendChild(calc_row);
